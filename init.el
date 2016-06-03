c6ae889f92cdfbecf893d88f4f8c49cc31b271b2
Nathan Sizemore <nathanrsizemore@gmail.com>
2016-06-02 21:50:25 -0400

    Customizations updated.

---
+++ init.el
@@ -0,0 +1,61 @@
+;; Nathan's bad-ass emacs config
+
+
+;; Human info
+(setq user-full-name "Nathan Sizemore")
+(setq user-mail-address "nathanrsizemore@gmail.com")
+
+;; Define package repositories
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("tromey" . "http://tromey.com/elpa/") t)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+	     '("gnu" . "http://elpa.gnu.org/packages/") t)
+
+;; Load and activate emacs packages
+(package-initialize)
+
+;; Download list of packages available from ELPA (Emacs Lisp Package Archive).
+;; Learn about it on http://www.emacswiki.org/emacs/ELPA
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Packages that must exist
+(defvar my-packages
+  '())
+
+;; On OS X, an Emacs instance started from the graphical user
+;; interface will have a different environment than a shell in a
+;; terminal window, because OS X does not run a shell during the
+;; login. Obviously this will lead to unexpected results when
+;; calling external utilities like make from Emacs.
+;; This library works around this problem by copying important
+;; environment variables from the user's shell.
+;; https://github.com/purcell/exec-path-from-shell
+(if (eq system-type 'darwin)
+  (add-to-list 'my-packages 'exec-path-from-shell))
+
+;; Iterate over the list of packages and install
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+;; Setup exec-path-from-shell package for Mac
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize)
+  (exec-path-from-shell-copy-envs
+   '("PATH" "CARGO_HOME" "RUST_SRC_PATH" "JAVA_HOME")))
+
+;; Customizations folder
+(defvar my-customizations-folder
+  (concat user-emacs-directory "customizations/"))
+;; Customizations
+(defvar my-customizations
+  '())
+;; Load customizations
+(dolist (c my-customizations)
+  (load (concat my-customizations-folder c ".el")))
