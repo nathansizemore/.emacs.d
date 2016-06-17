cb1e11b1cd257914d60f16896a8285a6c40baa4f
Nathan Sizemore <nathanrsizemore@gmail.com>
2016-06-17 09:28:11 -0400

    Dunno what this is.

--- customizations/misc.el
+++ customizations/misc.el
@@ -3,8 +3,9 @@
 ;; Changes all yes/no questions to y/n type
 (fset 'yes-or-no-p 'y-or-n-p)
 ;;
-;; No need for ~ files when editing
+;; No need for # or ~ files when editing
 (setq create-lockfiles nil)
+(setq make-backup-files nil)
 ;;
 ;; Delete trailing whitespace
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
