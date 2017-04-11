ed5fddf66e20d2125af9a5933f051c152e960c68
Nathan Sizemore <nathanrsizemore@gmail.com>
2017-04-10 22:15:39 -0400

    Linux-home adjustments.

--- customizations/markdown.el
+++ customizations/markdown.el
@@ -4,3 +4,4 @@
 (add-to-list 'load-path "~/.emacs.d/elpa")
 (autoload 'gfm-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist (cons (rx ".md" eos) 'gfm-mode))
+(add-to-list 'auto-mode-alist (cons (rx ".markdown" eos) 'gfm-mode))
