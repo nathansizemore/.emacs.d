941e3d431f274484038aacac9e34788421a0e23f
Nathan Sizemore <nathanrsizemore@gmail.com>
2017-05-31 09:57:52 -0400

    .Net web extensions.

--- customizations/web.el
+++ customizations/web.el
@@ -1,7 +1,13 @@
 ;; web-mode settings
-(add-to-list 'auto-mode-alist (cons (rx ".cshtml" eos) 'web-mode))
 (add-to-list 'auto-mode-alist (cons (rx ".asp" eos) 'web-mode))
+(add-to-list 'auto-mode-alist (cons (rx ".aspx" eos) 'web-mode))
+(add-to-list 'auto-mode-alist (cons (rx ".ascx" eos) 'web-mode))
+(add-to-list 'auto-mode-alist (cons (rx ".cshtml" eos) 'web-mode))
+(add-to-list 'auto-mode-alist (cons (rx ".Master" eos) 'web-mode))
 
 (setq web-mode-engines-alist
-      '(("razor"    . "\\.cshtml\\'"))
+      '(("razor"    . "\\.cshtml\\'")
+        ("aspx"    . "\\.aspx\\'")
+        ("aspx"    . "\\.ascx\\'")
+        ("aspx"    . "\\.Master\\'"))
 )
