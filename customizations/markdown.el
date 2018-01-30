ca3596199be5f48fd26acb5f00836bc28558669e
Nathan Sizemore <nathanrsizemore@gmail.com>
2018-01-30 13:28:39 -0500

    Font changes (I have glasses now).

--- customizations/markdown.el
+++ customizations/markdown.el
@@ -5,3 +5,8 @@
 (autoload 'gfm-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist (cons (rx ".md" eos) 'gfm-mode))
 (add-to-list 'auto-mode-alist (cons (rx ".markdown" eos) 'gfm-mode))
+
+;; Trailing whitespace is important in Markdown.
+(add-hook 'gfm-mode-hook
+          (lambda ()
+            (remove-hook 'before-save-hook 'delete-trailing-whitespace t)))
