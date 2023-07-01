25d08f3636b9ae443acaa693245fd606a758b4cf
Nathan Sizemore <nathanrsizemore@gmail.com>
2023-07-01 09:13:55 -0400

    clang-format col nums

--- lang/c.el
+++ lang/c.el
@@ -1,3 +1,24 @@
+(use-package clang-format
+  :ensure
+  :config
+  (setq clang-format-style "file")
+  )
+
+(defun clang-format-save-hook-for-this-buffer ()
+  "Create a buffer local save hook."
+  (add-hook 'before-save-hook
+            (lambda ()
+              (when (locate-dominating-file "." ".clang-format")
+                (clang-format-buffer))
+              ;; Continue to save.
+              nil)
+            nil
+            ;; Buffer local hook.
+            t))
+
 (add-hook 'c-mode-hook 'lsp)
 (add-hook 'c++-mode-hook 'lsp)
+(add-hook 'c-mode-hook (lambda () (clang-format-save-hook-for-this-buffer)))
+(add-hook 'c++-mode-hook (lambda () (clang-format-save-hook-for-this-buffer)))
+(add-hook 'glsl-mode-hook (lambda () (clang-format-save-hook-for-this-buffer)))
 
